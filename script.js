5408bf891b72fa6f9be1862ba3c39fc09180731c
jeetu yadav <jeetuyadav2091@gmail.com>
2025-12-25 21:03:53 +0530

    Implement certificate fullscreen viewer
    Added certificate fullscreen viewer functionality.

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ setInterval(() => {
   text.textContent = words[i];
   i = (i + 1) % words.length;
 }, 1500);
+// Certificate Fullscreen Viewer
+const certImages = document.querySelectorAll(".cert-box img");
+const viewer = document.getElementById("imgViewer");
+const viewerImg = document.getElementById("viewerImg");
+const closeViewer = document.getElementById("closeViewer");
+
+certImages.forEach(img => {
+  img.addEventListener("click", () => {
+    viewer.style.display = "flex";
+    viewerImg.src = img.src;
+  });
+});
+
+closeViewer.addEventListener("click", () => {
+  viewer.style.display = "none";
+});
+
+viewer.addEventListener("click", (e) => {
+  if (e.target !== viewerImg) viewer.style.display = "none";
+});
